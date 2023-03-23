2f5a86999141bacf5996213dd5c68d6863b67aff
Esociety <esocietyhere@gmail.com>
2023-03-23 13:39:54 +0100

    Update devtools

--- src/cli/devtools.rs
+++ src/cli/devtools.rs
@@ -1,3 +1,4 @@
+use ansi_term::Colour;
 use std::process::Command;
 
 use clap::Parser;
@@ -5,96 +6,58 @@ use clap::Parser;
 /// Install tarmac, remodel, rojo, wally, selene, and stylua
 #[derive(Debug, Parser)]
 pub struct DevtoolsCommand;
+
 impl DevtoolsCommand {
     pub fn run(&self) -> anyhow::Result<Option<String>> {
         devtools()
     }
 }
 
+pub struct Tool {
+    pub name: String,
+    pub version: String,
+}
+
+impl Tool {
+    pub fn new(name: &str, version: &str) -> Self {
+        Self {
+            name: name.to_string(),
+            version: version.to_string(),
+        }
+    }
+}
+
 fn devtools() -> anyhow::Result<Option<String>> {
-    // Trust roblox/tarmac
-    Command::new("aftman")
-        .arg("trust")
-        .arg("roblox/tarmac")
-        .output()
-        .expect("Failed to execute command");
+    let tools = vec![
+        Tool::new("roblox/tarmac", "0.7.0"),
+        Tool::new("rojo-rbx/remodel", "0.11.0"),
+        Tool::new("rojo-rbx/rojo", "7.2.1"),
+        Tool::new("upliftgames/wally", "0.3.1"),
+        Tool::new("kampfkarren/selene", "0.25.0"),
+        Tool::new("johnnymorganz/stylua", "0.17.0"),
+    ];
 
-    // Add roblox/tarmac@0.7.0
-    Command::new("aftman")
-        .arg("add")
-        .arg("roblox/tarmac@0.7.0")
-        .output()
-        .expect("Failed to execute command");
+    for tool in tools {
+        // Trust tool
+        Command::new("aftman")
+            .arg("trust")
+            .arg(tool.name.clone())
+            .output()
+            .expect("Failed to execute command");
 
-    // Trust rojo-rbx/remodel
-    Command::new("aftman")
-        .arg("trust")
-        .arg("rojo-rbx/remodel")
-        .output()
-        .expect("Failed to execute command");
+        // Add tool
+        Command::new("aftman")
+            .arg("add")
+            .arg(format!("{}@{}", tool.name, tool.version))
+            .output()
+            .expect("Failed to execute command");
 
-    // Add rojo-rbx/remodel@0.11.0
-    Command::new("aftman")
-        .arg("add")
-        .arg("rojo-rbx/remodel@0.11.0")
-        .output()
-        .expect("Failed to execute command");
-
-    // Trust rojo-rbx/rojo
-    Command::new("aftman")
-        .arg("trust")
-        .arg("rojo-rbx/rojo")
-        .output()
-        .expect("Failed to execute command");
-
-    // Add rojo-rbx/rojo@7.2.1
-    Command::new("aftman")
-        .arg("add")
-        .arg("rojo-rbx/rojo@7.2.1")
-        .output()
-        .expect("Failed to execute command");
-
-    // Trust upliftgames/wally
-    Command::new("aftman")
-        .arg("trust")
-        .arg("upliftgames/wally")
-        .output()
-        .expect("Failed to execute command");
-
-    // Add upliftgames/wally@0.3.1
-    Command::new("aftman")
-        .arg("add")
-        .arg("upliftgames/wally@0.3.1")
-        .output()
-        .expect("Failed to execute command");
-
-    // Trust kampfkarren/selene
-    Command::new("aftman")
-        .arg("trust")
-        .arg("kampfkarren/selene")
-        .output()
-        .expect("Failed to execute command");
-
-    // Add kampfkarren/selene@0.25.0
-    Command::new("aftman")
-        .arg("add")
-        .arg("kampfkarren/selene@0.25.0")
-        .output()
-        .expect("Failed to execute command");
-
-    // Trust johnnymorganz/stylua
-    Command::new("aftman")
-        .arg("trust")
-        .arg("johnnymorganz/stylua")
-        .output()
-        .expect("Failed to execute command");
-
-    // Add johnnymorganz/stylua@0.25.0
-    Command::new("aftman")
-        .arg("add")
-        .arg("johnnymorganz/stylua@0.17.0")
-        .output()
-        .expect("Failed to execute command");
-
-    Ok(Some("Installed devtools.".to_string()))
+        println!(
+            "{} {} {}",
+            Colour::Green.paint("Installed"),
+            Colour::Yellow.paint(tool.name),
+            Colour::Green.paint(tool.version)
+        )
+    }
+    Ok(Some("Finished installing devtools.".to_string()))
 }
