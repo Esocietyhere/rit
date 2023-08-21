7964e7b878c50abc841d90bf85e71d1577616422
Esociety <esocietyhere@gmail.com>
2023-08-21 22:03:56 +0200

    Use roblox studio path

--- src/cli/open.rs
+++ src/cli/open.rs
@@ -1,5 +1,7 @@
 use crate::color::Color;
 use clap::Parser;
+use roblox_install::RobloxStudio;
+use std::env;
 use std::path::Path;
 
 /// Open a place file in Roblox Studio
@@ -25,9 +27,18 @@ pub fn open_place(file_path: Option<String>) -> Option<String> {
     let input = file_path.unwrap_or(format!("build/{}.rbxl", "default"));
     let path = Path::new(&input);
 
+    if env::var("BROWSER").is_err() {
+        env::set_var(
+            "BROWSER",
+            RobloxStudio::locate()
+                .expect("Couldn't locate Roblox Studio installation")
+                .application_path(),
+        );
+    }
+
     if path.exists() {
         println!("{}", open_output(input.clone()));
-        opener::open(path).expect("Couldn't open Roblox Studio");
+        opener::open_browser(path).expect("Couldn't open Roblox Studio");
     } else {
         return Some(format!("File {:?} does not exist!", path));
     }
